0e7a015b004e2524d21a341dc3192cc5cc94fd93
vics <vics@ebay.com>
2019-03-09 13:40:18 -0800

    use port 80

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
 
 const options = {
